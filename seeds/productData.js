d2550bd4844047b6dac17135359d7545b85c9d48
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-06-25 11:50:15 -0700

    Fixed error

--- seeds/productData.js
+++ seeds/productData.js
@@ -6,7 +6,7 @@ const productdata = [
     shortDescription: '2022 Kia Telluride',
     longDescription:
       '2022 Kia Telluride. Consumer Reports SUV of the year. Low miles. Great condition.',
-    price: '48000.00',
+    price: 48000.0,
     image: 'https://unsplash.com/photos/tzpA1l2_UQ8',
     category_id: 4,
     user_id: 1,
