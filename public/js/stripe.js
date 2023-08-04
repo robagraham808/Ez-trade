c8906f2d77842419ea966dd0f6b7661dcd26dad2
rau1avi1a <avila0118@icloud.com>
2023-08-03 22:15:14 -0700

    updated features

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -32,41 +32,43 @@ deleteCartItem();
 const test = "test"
 
 async function checkoutCart() {
-  let cartItems = await getCartItems();
-  const itemsData = await cartItems.json();
-  console.log('getCartItems', itemsData[1].product.product_name);
+
+  alert('Coming Soon!!')
+  // let cartItems = await getCartItems();
+  // const itemsData = await cartItems.json();
+  // console.log('getCartItems', itemsData[1].product.product_name);
 
 
-  for (let i = 0; i < itemsData.length; i++) {
-    itemsData[i].product = itemsData[i].product.price;
-    // itemsData[i].user = itemsData[i].product.product_name;
+  // for (let i = 0; i < itemsData.length; i++) {
+  //   itemsData[i].product = itemsData[i].product.price;
+  //   // itemsData[i].user = itemsData[i].product.product_name;
 
-  };
+  // };
 
 
 
-  fetch("/api/checkout", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    // Send along all the information about the items
-    body: JSON.stringify({
-      items: itemsData,
-    }),
-  })
-    .then(res => {
-      if (res.ok) return res.json()
-      // If there is an error then make sure we catch that
-      return res.json().then(e => Promise.reject(e))
-    })
-    .then(({ url }) => {
-      // On success redirect the customer to the returned URL
-      window.location = url
-    })
-    .catch(e => {
-      console.log(e)
-    })
+  // fetch("/api/checkout", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   // Send along all the information about the items
+  //   body: JSON.stringify({
+  //     items: itemsData,
+  //   }),
+  // })
+  //   .then(res => {
+  //     if (res.ok) return res.json()
+  //     // If there is an error then make sure we catch that
+  //     return res.json().then(e => Promise.reject(e))
+  //   })
+  //   .then(({ url }) => {
+  //     // On success redirect the customer to the returned URL
+  //     window.location = url
+  //   })
+  //   .catch(e => {
+  //     console.log(e)
+  //   })
 }
 
 window.addEventListener('DOMContentLoaded', function () {
